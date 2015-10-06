1421c0cc334c3aee15917030482250b4dccfe653
Chandrasekhar Ramakrishnan <ciyer@illposed.com>
2015-10-06 11:11:56 +0200

    Extracted stats to a react component

--- js/app/app.js
+++ js/app/app.js
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(["d3/d3", "react/react", "app/filters"], function(d3, React, filters) {
+define(["d3/d3", "react/react", "app/filters", "app/stats"], function(d3, React, filters, stats) {
 
   var dateParser = d3.time.format("%Y-%m-%d");
   var dateFormatter = d3.time.format("%b %d %Y");
@@ -280,149 +280,6 @@ define(["d3/d3", "react/react", "app/filters"], function(d3, React, filters) {
       .attr("class", "img-responsive")
   };
 
-  OaiPresenter.prototype.drawChart = function(container, hist, bins, labels) {
-
-    var margin = this.chartMargin;
-    var numBins = bins.length - 1;
-    var width = this.chartWidth, height = this.chartHeight;
-
-    var blockWidth = Math.floor(width / numBins);
-    var y = d3.scale.linear()
-      .domain([0, d3.max(hist, function(d) { return d.y })])
-      .range([height, 0]);
-    var svg = container.selectAll("svg").data([0]);
-    svg.enter().append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var barg = svg.selectAll("g.bar")
-      .data(hist);
-    barg.enter()
-      .append("g")
-        .attr("class", "bar")
-        .attr("transform", function(d, i) { return "translate(" + (i * blockWidth) + "," + y(d.y) + ")"; });
-    barg.transition().duration(1000)
-      .attr("transform", function(d, i) { return "translate(" + (i * blockWidth) + "," + y(d.y) + ")"; });
-
-    var bar = barg.selectAll("rect")
-      .data(function(d) { return [d] });
-    bar.enter()
-      .append("rect")
-      .attr("x", 1)
-      .attr("y", function(d) { return height - y(d.y) })
-      .attr("width", blockWidth - 1)
-      .attr("height", 1);
-    bar.transition().duration(1000)
-      .attr("y", 0)
-      .attr("height", function(d) { return height - y(d.y) });
-
-    var textData = labels.map(function(d, i) { return {bar: hist[i], label: d} });
-    var textg = svg.selectAll("g.graphlabel")
-      .data(textData);
-
-    var shouldRotate = labels[0].length > 2;
-    function textTransform(d, i) {
-      var yTranslate = (height - 2);
-      var xTranslate = blockWidth * (i + 0.5);
-      if (shouldRotate) xTranslate += 4;
-      return "translate(" + xTranslate + "," + yTranslate + ")";
-    }
-
-    var textgEnter = textg.enter().append("g")
-      .attr("class", "graphlabel")
-      .attr("transform", textTransform);
-
-  //  CHART FILTERING
-  //    .on("click", function(d, i) { console.log(hist); });
-
-    var text = textg.selectAll("text")
-      .data(function(d, i) { return [d] });
-    var textEnter = text.enter()
-        .append("text");
-    (shouldRotate) ?
-      textEnter.attr("transform", "rotate(-90)") :
-      textEnter.attr("text-anchor", "middle");
-
-    text.text(function(d) { return d.label });
-
-  };
-
-  OaiPresenter.prototype.drawPriceInfo = function() {
-    function pricesToConsider(list) {
-      return list
-        .filter(function(d) { return "FALSE" == d["Historic"]})
-        .map(function(d) { return d["Price"] })
-        .filter(function(d) { return d > 0 });
-    }
-    var products = this.model.filteredProducts;
-    var prices = pricesToConsider(products);
-    var sortedPrices = prices.sort(d3.ascending);
-    var allProducts = this.model.products;
-    var allPrices = pricesToConsider(allProducts);
-    var allSortedPrices = allPrices.sort(d3.ascending);
-
-    var quartiles = [0.0, 0.25, 0.5, 0.75, 1.0];
-    var quintiles = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0];
-    var bins = quintiles.map(function(q) { return d3.quantile(allSortedPrices, q) - 1 });
-    bins[bins.length - 1] = bins[bins.length - 1] + 1;
-    var hist = d3.layout.histogram().bins(bins)(sortedPrices);
-    var labels = [];
-    var numberFormat = d3.format(".0f")
-    bins.reduce(function(prev, next) { labels.push("$" + numberFormat(prev) + " - " + (numberFormat(next) - 1)); return next; });
-
-    this.drawChart(this.priceContainer, hist, bins, labels);
-
-    var median = d3.median(prices);
-    median = d3.format("3.0f")(median);
-    var medianContainer = this.priceMedianContainer.data([median]);
-    medianContainer.text(function(d) { return "Median: $" + d });
-  };
-
-  OaiPresenter.prototype.drawMonthInfo = function() {
-    function monthsToConsider(list) {
-      return list
-        .filter(function(d) { return "FALSE" == d["Historic"] && null != d.releaseDate})
-        .map(function(d) { return d.releaseDate.getMonth() });
-    }
-    var labels = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"]
-    var months = monthsToConsider(this.model.filteredProducts);
-    var bins = [0,1,2,3,4,5,6,7,8,9,10,11,12];
-    var hist = d3.layout.histogram().bins(bins)(months);
-    this.drawChart(this.monthContainer, hist, bins, labels);
-  };
-
-  OaiPresenter.prototype.drawWeekdayInfo = function() {
-    function daysToConsider(list) {
-      return list
-        .filter(function(d) { return "FALSE" == d["Historic"] && null != d.releaseDate})
-        .map(function(d) { return d.releaseDate.getDay() });
-    }
-
-    var labels = ["S", "M", "T", "W", "T", "F", "S"];
-    var days = daysToConsider(this.model.filteredProducts);
-    var bins = [0,1,2,3,4,5,6,7];
-    var hist = d3.layout.histogram().bins(bins)(days);
-    this.drawChart(this.weekdayContainer, hist, bins, labels);
-  };
-
-  // CHART FILTERING -- Use this version of draw weekday info to get the actual items as the data in the histogram
-  OaiPresenter.prototype.drawWeekdayInfoValuer = function() {
-    function daysToConsider(list) {
-      return list
-        .filter(function(d) { return "FALSE" == d["Historic"] && null != d.releaseDate});
-    }
-
-    function valuer(d) { return d.releaseDate.getDay(); }
-
-    var labels = ["S", "M", "T", "W", "T", "F", "S"];
-    var days = daysToConsider(this.model.filteredProducts);
-    var bins = [0,1,2,3,4,5,6,7];
-    var hist = d3.layout.histogram().value(valuer).bins(bins)(days);
-    this.drawChart(this.weekdayContainer, hist, bins, labels);
-  };
-
   OaiPresenter.prototype.updateEndDateInfo = function() {
     var lastEntry = this.model.products[0]
     var lastEntryDate = lastEntry.releaseDate
@@ -432,9 +289,10 @@ define(["d3/d3", "react/react", "app/filters"], function(d3, React, filters) {
 
   OaiPresenter.prototype.update = function() {
     this.drawProductTable();
-    this.drawPriceInfo();
-    this.drawMonthInfo();
-    this.drawWeekdayInfo();
+    React.render(stats.stats({
+        model: model, presenter: presenter,
+        chartMargin: this.chartMargin, chartWidth: this.chartWidth, chartHeight: this.chartHeight
+      }), $("#stats-container")[0]);
     this.updateEndDateInfo();
   }
 
---
+++ js/app/stats.js
@@ -0,0 +1,206 @@
+/*!
+ * Independent Outlier Archive
+ *
+ * Copyright (c) 2014, 2015 Chandrasekhar Ramakrishnan
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+define(["react/react"], function(React) {
+
+  /**
+   * @function Unpack data from a join
+   */
+  function unpack(d) { return d;}
+
+  var StatsClass = React.createClass({
+
+    drawChart: function(container, hist, bins, labels) {
+
+      var margin = this.props.chartMargin;
+      var numBins = bins.length - 1;
+      var width = this.props.chartWidth, height = this.props.chartHeight;
+
+      var blockWidth = Math.floor(width / numBins);
+      var y = d3.scale.linear()
+        .domain([0, d3.max(hist, function(d) { return d.y })])
+        .range([height, 0]);
+      var svg = container.selectAll("svg").data([0]);
+      svg.enter().append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var barg = svg.selectAll("g.bar")
+        .data(hist);
+      barg.enter()
+        .append("g")
+          .attr("class", "bar")
+          .attr("transform", function(d, i) { return "translate(" + (i * blockWidth) + "," + y(d.y) + ")"; });
+      barg.transition().duration(1000)
+        .attr("transform", function(d, i) { return "translate(" + (i * blockWidth) + "," + y(d.y) + ")"; });
+
+      var bar = barg.selectAll("rect")
+        .data(function(d) { return [d] });
+      bar.enter()
+        .append("rect")
+        .attr("x", 1)
+        .attr("y", function(d) { return height - y(d.y) })
+        .attr("width", blockWidth - 1)
+        .attr("height", 1);
+      bar.transition().duration(1000)
+        .attr("y", 0)
+        .attr("height", function(d) { return height - y(d.y) });
+
+      var textData = labels.map(function(d, i) { return {bar: hist[i], label: d} });
+      var textg = svg.selectAll("g.graphlabel")
+        .data(textData);
+
+      var shouldRotate = labels[0].length > 2;
+      function textTransform(d, i) {
+        var yTranslate = (height - 2);
+        var xTranslate = blockWidth * (i + 0.5);
+        if (shouldRotate) xTranslate += 4;
+        return "translate(" + xTranslate + "," + yTranslate + ")";
+      }
+
+      var textgEnter = textg.enter().append("g")
+        .attr("class", "graphlabel")
+        .attr("transform", textTransform);
+
+    //  CHART FILTERING
+    //    .on("click", function(d, i) { console.log(hist); });
+
+      var text = textg.selectAll("text")
+        .data(function(d, i) { return [d] });
+      var textEnter = text.enter()
+          .append("text");
+      (shouldRotate) ?
+        textEnter.attr("transform", "rotate(-90)") :
+        textEnter.attr("text-anchor", "middle");
+
+      text.text(function(d) { return d.label });
+
+    },
+
+    drawPriceInfo: function(products, allProducts, priceContainer, priceMedianContainer) {
+      function pricesToConsider(list) {
+        return list
+          .filter(function(d) { return "FALSE" == d["Historic"]})
+          .map(function(d) { return d["Price"] })
+          .filter(function(d) { return d > 0 });
+      }
+      var prices = pricesToConsider(products);
+      var sortedPrices = prices.sort(d3.ascending);
+      var allPrices = pricesToConsider(allProducts);
+      var allSortedPrices = allPrices.sort(d3.ascending);
+
+      var quartiles = [0.0, 0.25, 0.5, 0.75, 1.0];
+      var quintiles = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0];
+      var bins = quintiles.map(function(q) { return d3.quantile(allSortedPrices, q) - 1 });
+      bins[bins.length - 1] = bins[bins.length - 1] + 1;
+      var hist = d3.layout.histogram().bins(bins)(sortedPrices);
+      var labels = [];
+      var numberFormat = d3.format(".0f")
+      bins.reduce(function(prev, next) { labels.push("$" + numberFormat(prev) + " - " + (numberFormat(next) - 1)); return next; });
+
+      this.drawChart(priceContainer, hist, bins, labels);
+
+      var median = d3.median(prices);
+      median = d3.format("3.0f")(median);
+      var medianContainer = priceMedianContainer.data([median]);
+      medianContainer.text(function(d) { return "Median: $" + d });
+    },
+
+    drawMonthInfo: function(products, monthContainer) {
+      function monthsToConsider(list) {
+        return list
+          .filter(function(d) { return "FALSE" == d["Historic"] && null != d.releaseDate})
+          .map(function(d) { return d.releaseDate.getMonth() });
+      }
+      var labels = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"]
+      var months = monthsToConsider(products);
+      var bins = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+      var hist = d3.layout.histogram().bins(bins)(months);
+      this.drawChart(monthContainer, hist, bins, labels);
+    },
+
+    drawWeekdayInfo: function(products, weekdayContainer) {
+      function daysToConsider(list) {
+        return list
+          .filter(function(d) { return "FALSE" == d["Historic"] && null != d.releaseDate})
+          .map(function(d) { return d.releaseDate.getDay() });
+      }
+
+      var labels = ["S", "M", "T", "W", "T", "F", "S"];
+      var days = daysToConsider(products);
+      var bins = [0,1,2,3,4,5,6,7];
+      var hist = d3.layout.histogram().bins(bins)(days);
+      this.drawChart(weekdayContainer, hist, bins, labels);
+    },
+
+    // CHART FILTERING -- Use this version of draw weekday info to get the actual items as the data in the histogram
+    drawWeekdayInfoValuer: function(products, weekdayContainer) {
+      function daysToConsider(list) {
+        return list
+          .filter(function(d) { return "FALSE" == d["Historic"] && null != d.releaseDate});
+      }
+
+      function valuer(d) { return d.releaseDate.getDay(); }
+
+      var labels = ["S", "M", "T", "W", "T", "F", "S"];
+      var days = daysToConsider(products);
+      var bins = [0,1,2,3,4,5,6,7];
+      var hist = d3.layout.histogram().value(valuer).bins(bins)(days);
+      this.drawChart(weekdayContainer, hist, bins, labels);
+    },
+
+    componentDidMount: function() {
+      // Just update the component
+      this.componentDidUpdate();
+    },
+
+    componentDidUpdate: function() {
+      var products = this.props.model.filteredProducts;
+      var allProducts = this.props.model.products;
+
+      this.drawPriceInfo(products, allProducts, d3.select("#price-container"), d3.select("#price-median-container"));
+      this.drawMonthInfo(products, d3.select("#month-container"));
+      this.drawWeekdayInfo(products, d3.select("#weekday-container"));
+    },
+
+    render: function() {
+      var statsStructure = [
+        {name: "Price", subs: ["price-container", "price-median-container"]},
+        {name: "Month", subs: ["month-container"]},
+        {name: "Weekday", subs: ["weekday-container"]}
+      ];
+      var stats = statsStructure.map(function(d) {
+        var title = React.DOM.h4(null, d.name);
+        var statElts = d.subs.map(function(s) { return React.DOM.span({id: s}) });
+        statElts.unshift(title);
+        return React.DOM.div({id: d, className: 'col-xs-4 col-md-4', style: {width: '110px'}}, statElts);
+      });
+
+      var statsGroup = React.DOM.div({className:'row'}, stats);
+      var title = React.DOM.h3(null, 'Stats');
+      var column = React.DOM.div({className: 'col-xs-6 col-md-6'}, [title, statsGroup]);
+      return column;
+    }
+  });
+
+  var Stats = React.createFactory(StatsClass)
+
+  return { stats: Stats }
+})
