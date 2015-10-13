310d5f16dad83079876f4bd5f952301b8b67f7c6
Chandrasekhar Ramakrishnan <ciyer@illposed.com>
2015-10-13 10:06:17 +0200

    Update to about

--- js/app/about.js
+++ js/app/about.js
@@ -16,15 +16,23 @@ var Guide = React.createClass({
   render: function() {
     return (<div className="col-md-8">
               <h3>Guide</h3>
-                <div id="guide-text">
+                <h4>Use</h4>
                   <p>The clothsography shows most <a href="http://www.outlier.cc">Outlier</a> releases made in the period from April 2011 to {this.props.endmonth} {this.props.endyear}, and a few from earlier as well.</p>
                   <p>
                     To focus on certain kinds of items, turn on one or more of the filters from the far left. For example, to see just shirts for women, expand <strong>Clothes</strong> and select <strong>Shirts</strong> then expand <strong>Men/Women/Unisex</strong> and select <strong>Women</strong>.
                   </p>
+                  <h4>Release Frequency Stats</h4>
                   <p>
-                    The data on release dates is from <a href="http://milled.com/outlier">Milled/Outlier</a> and <a href="https://twitter.com/outlier">twitter</a>; price data is mostly from the Outlier web site; any mistakes are mine alone. If you spot something, let <a href="https://twitter.com/ciyer">me know</a>.
+                    For individual products, I compute statistics on release frequency. For this purpose, I ignore releases before 2013 because releases were less frequent then.
                   </p>
-                </div>
+                  <h4>Data Sources</h4>
+                  <p>
+                    The data on release dates is from <a href="http://milled.com/outlier">Milled/Outlier</a>, <a href="https://twitter.com/outlier">twitter</a>, and <a href="https://reddit.com/r/outlier">reddit</a>; price data is mostly from the Outlier web site; any mistakes are mine alone. If you spot something, let <a href="https://twitter.com/ciyer">me (@ciyer) know</a>.
+                  </p>
+                  <h4>Code</h4>
+                  <p>
+                    The code is available on <a href="https://github.com/ciyer/outlier">GitHub</a>, in particular the data is available as <a href="https://github.com/ciyer/outlier/blob/master/outlier-data.csv">outlier-data.csv</a>.
+                  </p>
             </div>)
   }
 });
